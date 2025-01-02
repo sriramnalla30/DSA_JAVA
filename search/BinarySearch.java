fe5203f26391e9d6e12ee4d4bee5020da0110a92
sriramnalla30 <sriramnalla30@gmail.com>
2025-01-02 18:47:35 +0530

    Auto-update: 02-01-2025 18:47:35.21

--- search/BinarySearch.java
+++ search/BinarySearch.java
@@ -3,10 +3,12 @@ class BinarySearch{
         int nums[]={1,3,5,6,7,11};
         int target=7;
         int result=Binarysearch(nums,target);
+        int result2=BinaryRec(nums,target,0,nums.length);
         if(result==-1){
             System.out.println("not found");
         }else{
             System.out.println("Number found at : "+result);
+            System.out.println("Number found at : "+result2);
         }
     }
     public static int Binarysearch(int[] nums,int target){
@@ -26,6 +28,21 @@ class BinarySearch{
         
     }
         return -1;
+    }
+    public static int BinaryRec(int[] nums,int target,int left,int right){
+        if (left > right) {
+            return -1; // Base case: target not found
+        }
+        int mid =(left+right)/2;
+        if(nums[mid] ==target){
+            return mid;
+        }
+        else if(nums[mid]<target){
+           return BinaryRec(nums,target,mid+1,right);
+
+        }
+        else return BinaryRec(nums,target,left,mid-1);
+
     }
 }
 //Binary Search time complexity  best = o(1) worst=O(log n)  avg=O(log n) ------------space complexity o(1)
